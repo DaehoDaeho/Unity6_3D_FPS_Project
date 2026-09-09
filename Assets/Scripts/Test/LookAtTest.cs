775d3d7b616991a394d417c7686aa3b13748f21b
DaehoDaeho <relayer@naver.com>
2026-09-09 20:08:51 +0900

    .

---
+++ Assets/Scripts/Test/LookAtTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LookAtTest : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+    }
+}
